a3e750b201f03d12ecdea184a8ed72551b436664
GaSab <gabrielsaboya1201@gmail.com>
2024-07-24 15:00:29 -0300

    query to retrieve sugested items

--- apimeliuz/salesdata/views.py
+++ apimeliuz/salesdata/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from models import SalesData
+from queries.retrive_most_sold import retrieve_query
 
 # Create your views here.
+def new_user(request):
+    if request.method == 'GET':
+        SalesData.object.raw(retrieve_query)
+        return None
