eefb450c19b7389338877de15d8ca4403c08f678
GaSab <gabrielsaboya1201@gmail.com>
2024-07-24 14:13:21 -0300

    create and fill database

--- apimeliuz/salesdata/models.py
+++ apimeliuz/salesdata/models.py
@@ -3,14 +3,18 @@ from django.db import models
 # Create your models here.
 class SalesData(models.Model):
     sale_date = models.CharField(
-        
+        max_length=20
     )
-product_id= models.IntegerField(
-    
-),
-product_title,
-product_price,
-product_image_url,
-store_name,
-store_id,
-sales_per_day
+    product_id = models.IntegerField()
+    product_title = models.CharField(
+       max_length=100
+    )
+    product_price = models.FloatField()
+    product_image_url = models.CharField(
+        max_length=100
+    )
+    store_name = models.CharField(
+       max_length=50
+    )
+    store_id = models.IntegerField()
+    sales_per_day = models.IntegerField()  
