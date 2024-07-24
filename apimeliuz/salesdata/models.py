b2fdda9396e1b9f8c312786a851e510840d5f8e5
GaSab <gabrielsaboya1201@gmail.com>
2024-07-24 12:08:49 -0300

    created app and data exploration in jupyter

---
+++ apimeliuz/salesdata/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class SalesData(models.Model):
+    sale_date = models.CharField(
+        
+    )
+product_id= models.IntegerField(
+    
+),
+product_title,
+product_price,
+product_image_url,
+store_name,
+store_id,
+sales_per_day
